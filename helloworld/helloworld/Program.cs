cad9116d13d3ae99b552e7af4570a13b27c7c0a0
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-22 15:13:55 -0800

    first commit for c# projects. said hello world

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme accounting \n were \"accounting\" on you!");
+        }
+    }
+
