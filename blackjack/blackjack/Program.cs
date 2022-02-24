fbe4c91b5323a81f437639d985a8ab6c2ce33a3c
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-23 19:35:24 -0800

    did some stuff

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -9,34 +9,15 @@ namespace blackjack
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck, out int timesShuffled, 2);
-            Console.WriteLine(timesShuffled);
+            deck.Shuffle(2);
+            
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
         }
-        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
-        {
-            timesShuffled = 0;
-            for (int i = 0; i < times; i++)
-            {
-                timesShuffled++;
-                List<Card> tempList = new List<Card>();
-                Random random = new Random();
-
-                while (deck.Cards.Count > 0)
-                {
-                    int randomIndex = random.Next(0, deck.Cards.Count);
-                    tempList.Add(deck.Cards[randomIndex]);
-                    deck.Cards.RemoveAt(randomIndex);
-                }
-                deck.Cards = tempList;
-            }
-
-            return deck;
-        }
+        
         
     }
 }
