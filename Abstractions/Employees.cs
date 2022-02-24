305855c8f167fde9fb35c6f4de0d22cac0e7bb6f
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-24 14:16:00 -0800

    did some generic stuff

--- Abstractions/Employees.cs
+++ Abstractions/Employees.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 
-//Perform these actions and create a console app that includes the following:
+//
+//Make the Employee class take a generic type parameter.
+
+//Add a property to the Employee class called “things” and have its data type be a generic list
+//matching the generic type of the class.
 
 
-//Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
 
-//Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
-
-//Upload your code to GitHub and submit the link below:
+//Add comments to each line or block of your code to explain what it does exactly, 
+//so that another developer could read and understand your code.
 namespace Abstractions
 {
-    public class Employees : Person, IQuittable
+    public class Employees<T> : Person//, IQuittable
     {
+        public List<T> Things = new List<T>();
         public int Id { get; set; }
 
         public static bool IdsAreEqual { get; set; }
@@ -21,35 +24,35 @@ namespace Abstractions
         {
             base.SayName();
         }
-        public void Quit()
-        {
-            Console.WriteLine(FirstName + " says fuck this shit im outta here");
-        }
-        public static Employees operator== (Employees employee1, Employees employee2)
-        {
-            if(employee1.Id == employee2.Id)
-            {
-                throw new InvalidOperationException("employee id's cannot match");
+        //public void Quit()
+        //{
+        //    Console.WriteLine(FirstName + " says fuck this shit im outta here");
+        //}
+        //public static Employees operator== (Employees employee1, Employees employee2)
+        //{
+        //    if(employee1.Id == employee2.Id)
+        //    {
+        //        throw new InvalidOperationException("employee id's cannot match");
                 
-            }
-            else
-            {
-                return employee1;
-            }
-        }
-        public static Employees operator !=(Employees employee1, Employees employee2)
-        {
-            if (employee1.Id == employee2.Id)
-            {
-                return employee1;
+        //    }
+        //    else
+        //    {
+        //        return employee1;
+        //    }
+        //}
+        //public static Employees operator !=(Employees employee1, Employees employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //    {
+        //        return employee1;
 
-            }
-            else
-            {
-                throw new InvalidOperationException("employee id's cannot match");
+        //    }
+        //    else
+        //    {
+        //        throw new InvalidOperationException("employee id's cannot match");
                 
-            }
-        }
+        //    }
+        //}
 
     }
 }
--- Abstractions/Program.cs
+++ Abstractions/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-//Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”.
-//Call the SayName() method on the object.
+//Instantiate an Employee object with type “string” as its generic parameter. Assign a list of strings as
+//the property value of Things.
+
+//Instantiate an Employee object with type “int” as its generic parameter. Assign a list of integers as the
+//property value of Things.
+
+//Create a loop that prints all of the Things to the Console.
 namespace Abstractions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employees employee = new Employees { LastName = "Student", FirstName = "Sample", Id = 10 };
-            // calls parent Person method inside employee with instanciated employees information i think
-            //employee.SayName();
-            //employee.Quit();
-            Employees employee2 = new Employees { Id = 11 };
-            Console.WriteLine(employee == employee2);
+            //Employees employee = new Employees { LastName = "Student", FirstName = "Sample", Id = 10 };
+            //// calls parent Person method inside employee with instanciated employees information i think
+            ////employee.SayName();
+            ////employee.Quit();
+            //Employees employee2 = new Employees { Id = 11 };
+            //// custom comparison throws error if IDs match else returns the first employee
+            //Console.WriteLine(employee == employee2);
+            Employees<string> employee = new Employees<string>();
+            employee.Things.Add("strings");
+            employee.Things.Add("morbid");
+            employee.Things.Add("obesity");
+            employee.Things.Add("stings");
+            foreach (string item in employee.Things)
+            {
+                Console.WriteLine(item);
+            }
 
-            
+
+
         }
     }
 }
