b101e40937df75338b615927a789824dce7dc35a
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-25 14:38:16 -0800

    exceptional program

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -14,8 +14,18 @@ namespace blackjack
             
             Console.WriteLine("come play blackjack. what is your name");
             string playerName = Console.ReadLine();
-            Console.WriteLine("how much you going to put down");
-            int bank = Convert.ToInt32(Console.ReadLine());
+            int bank = 0;
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+                Console.WriteLine("how much you going to put down");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer)
+                {
+                    Console.WriteLine("enter an integer only");
+                }
+            }
+            
             Console.WriteLine("{0}, Do you want to play", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "y" || answer == "ya")
@@ -32,7 +42,20 @@ namespace blackjack
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("security is incoming");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("errors occurred try again later");
+                        return;
+                    }
+                    
                 }
                 game = game - player;
                 Console.WriteLine("thank you for playing");
