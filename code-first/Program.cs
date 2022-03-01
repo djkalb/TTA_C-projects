a65017d43522b9a3421753ab05e6d95afe41540a
djkalb <71739290+djkalb@users.noreply.github.com>
2022-02-28 16:04:13 -0800

    code first dB

---
+++ code-first/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace code_first
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            using (var ctx = new StudentContext())
+            {
+                var student = new Student() { 
+                    StudentName = "Bill Withers",
+                    DateOfBirth = DateTime.Now,
+                    Course = "Cursive"};
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ code-first/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace code_first
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
